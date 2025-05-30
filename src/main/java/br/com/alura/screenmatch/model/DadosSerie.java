7a4389d4811d7507eca3f6df9b28c1946b151266
ViniciusLBarbosa <vinilimab@gmail.com>
2025-05-30 11:03:36 -0300

    initial Commit

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -1,8 +1,10 @@
 package br.com.alura.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTempordas,
                          @JsonProperty("imdbRating") String avaliacao){
